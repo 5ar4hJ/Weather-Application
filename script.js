5c64c4dc54d740c52ae8bbd782bf02b435b80a2f
5ar4hJ <busybeesjn@gmail.com>
2024-03-16 23:47:59 -0600

    Added basic functionality using API

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+function updateData(response) {
+  let displayedTemp = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#displayed-city");
+
+  cityElement.innerHTML = response.data.city;
+  displayedTemp.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "o974f949a162ca8at386ecd74f5bc0de";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateData);
+}
+
 function displayData(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#displayed-city");
-  cityElement.innerHTML = searchInputElement.value;
+
+  searchCity(searchInputElement.value);
 }
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", displayData);
+
+searchCity("Calgary");
